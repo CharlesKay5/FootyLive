694f9e0b1484da16866cf2df476a190f66a91762
CharlesKay5 <CharlesKay5@outlook.com>
2024-06-10 14:16:16 +0930

    Removed sneaky puppeteer reference

--- public/dtliveChatAPI.js
+++ public/dtliveChatAPI.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require('path');
 const xml2js = require('xml2js');
-const puppeteer = require('puppeteer');
 const { response } = require("express");
 
 const data = {
